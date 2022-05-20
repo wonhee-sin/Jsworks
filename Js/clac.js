b4f5f793cb7b7b647f8ba500b0976db6223d776e
wonhee-sin <ww1700@naver.com>
2022-05-20 18:13:05 +0900

    fitst commit

---
+++ Js/clac.js
@@ -0,0 +1,15 @@
+// 덧셈 프로그램
+
+let n1 = prompt("첫 번째 수를 입력하세요" ,"1~45");
+
+n1 = parseInt(n1)
+
+let n2 = prompt("두 번째 수를 입력하세요" ,"1~45");
+
+n2 = parseInt(n2)
+
+sum = 0;
+
+sum = n1 + n2 ;
+
+document.write(sum);
