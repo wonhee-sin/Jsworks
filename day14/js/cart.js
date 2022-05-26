67500c1251a1621f81fca1632e155ff7f1b1be14
wonhee-sin <ww1700@naver.com>
2022-05-26 13:38:58 +0900

    nwe

---
+++ day14/js/cart.js
@@ -0,0 +1,23 @@
+//장바구니 배열 사용하기
+let cart = new Array(5);
+// console.log(cart.length + "개");
+cart = ["생수", "라면", "커피", "계란", "쌀"];
+
+//"커피"를 출력하세요
+
+// document.write(cart[2]);
+
+//"계란"을 사과로 변경하세요
+
+cart[3] = "사과";
+
+//"라면"을 삭제하세요.
+
+delete cart[1];
+
+//목록 전체를 출력하세요.
+for(let i = 0; i < cart.length ; i++){
+     document.write(cart[i] + " ");
+}
+
+console.log(cart);
