bf21bf055cd6d090f2353974cc3ed0d36961c8ee
wonhee-sin <ww1700@naver.com>
2022-05-27 18:18:40 +0900

    tgtF!

---
+++ day15/js/flower.js
@@ -0,0 +1,16 @@
+function showText(){
+     let text = document.getElementById('detail');
+     text.style.display = "block" ;
+
+     // let button = document.getElementById('show');
+     // button.style.display = "none";
+}
+
+function hideText(){
+     let text = document.getElementById('detail');
+     text.style.display = "none" ;
+
+     // let button = document.getElementById('show');
+     // button.style.display = "block";
+     //보기 버튼 보이기
+}
